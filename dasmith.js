39c6d47b28b27ece043af63a7bbd585d9e411441
zero-to-prod <61474950+zero-to-prod@users.noreply.github.com>
2020-09-23 10:52:52 -0400

    Update dasmith.js

--- dasmith.js
+++ dasmith.js
@@ -1,8 +1,10 @@
 /**
- * Returns a random number limited by the 'max_value'
+ * Returns a random number between 1 and the 'max_value'
+ * 
+ * Default value returns a number between 1 and 2
  *
  * @param {integer} max_value
  */
 function das_random_integer(max_value = 2) {
-  return Math.floor(Math.random() * max_value);
+  return Math.floor(Math.random() * max_value) + 1;
 }
